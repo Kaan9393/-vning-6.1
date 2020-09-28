45189a943199c900a3e64ae27d83a32f611fc4f1
Kaan9393 <71064062+Kaan9393@users.noreply.github.com>
2020-09-28 10:12:26 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,9 +8,16 @@ namespace InlämningsUppgift
         static void Main(string[] args)
         {
             List<Tjuv> bibliotekTjuv = new List<Tjuv>();
+
+            int X = 100;
+            int Y = 25;
+            String[,] spelplan = new string[X, Y];
+
+
             for (int t = 0; t < 20; t++)
             {
                 bibliotekTjuv.Add(new Tjuv(t, t, 1, 1));
+                Console.WriteLine(bibliotekTjuv);
             }
 
             List<Polis> bibliotekPolis = new List<Polis>();
@@ -25,134 +32,97 @@ namespace InlämningsUppgift
                 bibliotekMedborgare.Add(new Medborgare(m, m, 3, 3));
             }
 
-            for (int y = 0; y <= 25; y++)
-            {
-                Console.WriteLine(" ");
-              for (int x = 0; x < 100; x++)
-              {
-                    foreach (var t in bibliotekTjuv)
-                    {
-                        Console.Write(" ");
-                        Console.Write(t.GetBoardChar());
-                        t.Move();
+            Console.ReadKey();
 
-                    }
-                    foreach (var p in bibliotekPolis)
-                    {
-                        Console.Write(" ");
-                        Console.Write(p.GetBoardChar());
-                        p.Move();
-                    }
-                    foreach (var m in bibliotekMedborgare)
-                    {
-                        Console.Write(" ");
-                        Console.Write(m.GetBoardChar());
-                        m.Move();
-                    }
+
+
+        }
+
+
+
+        class Person
+        {
+            public int PositionX { get; set; }
+            public int PositionY { get; set; }
+            public int DirectionX { get; set; }
+            public int DirectionY { get; set; }
+            //Inventory? 
+
+            public void Move()
+            {
+                // Denna är för att Position X och Y ska ändras, för det behöver vi plussa DirectionX,Y.
+                PositionX = PositionX + DirectionX;
+                PositionY = PositionY + DirectionY;
+
+                if (PositionX < 0)
+                {
+                    PositionX = DirectionX;
                 }
-              Console.WriteLine();
+                if (PositionY < 0)
+                {
+                    PositionY = DirectionY;
+                }
             }
 
-            
-
-            //För att använda override här nere så kan jag lägga in p.GetBoardChar i foreach metoden
-
-            //for (int y = 0; y <= 25; y++)
-            //{
-            //    for (int x = 0; x < 100; x++)
-            //    {
-            //        Console.WriteLine("x");
-            //    }
-            //    Console.WriteLine();
-            //}
-     
-        }
-
-    }
-
-    class Person
-    {
-        public int PositionX { get; set; }
-        public int PositionY { get; set; }
-        public int DirectionX { get; set; }
-        public int DirectionY { get; set; }
-        //Inventory? 
-
-        public void Move()
-        {
-            // Denna är för att Position X och Y ska ändras, för det behöver vi plussa DirectionX,Y.
-            PositionX = PositionX + DirectionX;
-            PositionY = PositionY + DirectionY;
-
-            if (PositionX < 0)
+            public virtual char GetBoardChar()
             {
-                PositionX = DirectionX;
+                return '?';
             }
-            if (PositionY < 0)
+
+        }
+
+        class Tjuv : Person
+        {
+            public Tjuv(int positionX, int positionY, int directionX, int directionY)
             {
-                PositionY = DirectionY;
+                PositionX = positionX;
+                PositionY = positionY;
+                DirectionX = directionX;
+                DirectionY = directionY;
             }
+
+            public override char GetBoardChar()
+            {
+                return 'T';
+            }
+
+            //gör en metod med STEAL
         }
 
-        public virtual char GetBoardChar() 
+        class Polis : Person
         {
-            return '?';
+            public Polis(int positionX, int positionY, int directionX, int directionY)
+            {
+                PositionX = positionX;
+                PositionY = positionY;
+                DirectionX = directionX;
+                DirectionY = directionY;
+            }
+
+            public override char GetBoardChar()
+            {
+                return 'P';
+            }
+
+            ///Gör en metod med beslagtaget
         }
 
+        class Medborgare : Person
+        {
+            public Medborgare(int positionX, int positionY, int directionX, int directionY)
+            {
+                PositionX = positionX;
+                PositionY = positionY;
+                DirectionX = directionX;
+                DirectionY = directionY;
+            }
+
+            public override char GetBoardChar()
+            {
+                return 'M';
+            }
+
+        }
     }
-
-    class Tjuv : Person
-    {
-        public Tjuv(int positionX, int positionY, int directionX, int directionY)
-        {
-            PositionX = positionX;
-            PositionY = positionY;
-            DirectionX = directionX;
-            DirectionY = directionY;
-        }
-
-        public override char GetBoardChar()
-        {
-            return 'T';
-        }
-
-        //gör en metod med STEAL
-    }
-
-    class Polis : Person
-    {
-        public Polis(int positionX, int positionY, int directionX, int directionY)
-        {
-            PositionX = positionX;
-            PositionY = positionY;
-            DirectionX = directionX;
-            DirectionY = directionY;
-        }
-
-        public override char GetBoardChar()
-        {
-            return 'P';
-        }
-
-        ///Gör en metod med beslagtaget
-    }
-
-    class Medborgare : Person
-    {
-        public Medborgare(int positionX, int positionY, int directionX, int directionY)
-        {
-            PositionX = positionX;
-            PositionY = positionY;
-            DirectionX = directionX;
-            DirectionY = directionY;
-        }
-
-        public override char GetBoardChar()
-        {
-            return 'M';
-        }
-
-    }
-
 }
 
