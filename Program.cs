2b7abca48d6d7e4b747be6edd5c9bcae84ec44da
Kaan9393 <71064062+Kaan9393@users.noreply.github.com>
2020-10-22 20:22:39 +0200

    Båt

--- Program.cs
+++ Program.cs
@@ -1,126 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
-namespace Övning_Klädbutiken
+namespace InlämningUppgift
 {
     class Program
     {
+        static Random randomWeight = new Random();
+        static Random randomSpeed = new Random();
+        static Random randomRoddbåtPassenger = new Random();
+        static Random randomMotorbåtHorsePower = new Random();
+        static Random randomSegelbåtLenght = new Random();
+        static Random randomLastfartygContainers = new Random();
         static void Main(string[] args)
         {
-            Plagg plagg = default;
-
-            Console.Write("Tryck 1 för Pants: ");
-            Console.Write("Tryck 2 för Shirt: ");
-            Console.Write("Tryck 3 för Shoes: ");
-            int plaggTyp = int.Parse(Console.ReadLine());
-            plagg.Type = (Enum.GetName(typeof(Type), plaggTyp));
-
-            Console.Write("Tryck 1 för Xs: ");
-            Console.Write("Tryck 2 för S: ");
-            Console.Write("Tryck 3 för M: ");
-            int storlek = int.Parse(Console.ReadLine());
-            plagg.Size = (Enum.GetName(typeof(Size), storlek));
-
-            Console.Write("Tryck 1 för Blue: ");
-            Console.Write("Tryck 2 för Green: ");
-            Console.Write("Tryck 3 för Yellow: ");
-            int färg = int.Parse(Console.ReadLine());
-            plagg.Color = (Enum.GetName(typeof(Color), färg));
-
-            Console.Write("Tryck 1 för 100kr: ");
-            Console.Write("Tryck 2 för 200kr: ");
-            Console.Write("Tryck 3 för 300kr: ");
-            int pris = int.Parse(Console.ReadLine());
-            int one = (int)Price.OneHundred;
-            int two = (int)Price.TwoHundred;
-            int three = (int)Price.ThreeHundred;
-
-            if (pris == 1)
-            {
-                pris = one;
-            }
-            else if (pris == 2)
-            {
-                pris = two;
-            }
-            else if (pris == 3)
-            {
-                pris = three;
-            }
-            plagg.Price = (Enum.GetName(typeof(Price), pris));
-
-            List<Plagg> butik = new List<Plagg>();
-            butik.Add(plagg);
-
-
-            Console.WriteLine($"{plagg.Type}, {plagg.Size}, {plagg.Color}, {plagg.Price}");
             
 
-            //List<Type> type = new List<Type>();
+            //Roddbåt roddbåt = new Roddbåt(RandomID(), randomWeight.Next(100, 300), randomSpeed.Next(1, 3), randomRoddbåtPassenger.Next(1, 6));
 
-            //Console.Write("Lägg in TYP: Välj 1 för Pants, Välj 2 för Shirt, Välj 3 för Shoes ");
-            //string plaggTyp = Console.ReadLine();
-            //foreach (Type p in plaggTyp)
-            //{
-            //    Console.WriteLine(p);
-            //}
-            //Console.Write("Välj Plagg: ");
-            //string plaggTyp = Console.ReadLine();
+            //Motorbåt motorbåt = new Motorbåt(RandomID(), randomWeight.Next(200, 3000), randomSpeed.Next(1, 6), randomMotorbåtHorsePower.Next(10, 1000));
 
-            //Console.Write("Välj Storlek: ");
-            //string storlek = Console.ReadLine();
+            //Segelbåt segelbåt = new Segelbåt(RandomID(), randomWeight.Next(800, 6000), randomSpeed.Next(1, 12), randomSegelbåtLenght.Next(10, 60));
 
-            //Console.Write("Välj Färg: ");
-            //string färg = Console.ReadLine();
+            //Lastfartyg lastfartyg = new Lastfartyg(RandomID(), randomWeight.Next(3000, 20000), randomSpeed.Next(1, 20), randomLastfartygContainers.Next(1, 500));
 
-            //Console.Write("Välj Pris: ");
-            //int pris = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Plats\tBåttyp\tID\tVikt\tMaxhastighet\tÖvrigt");
+            Båt[] båtArray = new Båt[10];
 
-            //List<Plagg> clothes = new List<Plagg>();
 
-            Console.ReadKey();
         }
 
-    }
 
-    struct Plagg
-    {
-        public string Type { get; set; }
-        public string Size { get; set; }
-        public string Color { get; set; }
-        public string Price { get; set; }
 
-        public Plagg(string type, string size, string color, string price)
+
+
+
+        //To get Random ID for all Boats.
+        public static string RandomID()
         {
-            Type = type;
-            Size = size;
-            Color = color;
-            Price = price;
-        }
-    }
+            Random randomID = new Random();
+            string b = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int length = 3;
+            string random = "";
 
-    enum Type
-    {
-        Pants = 1,
-        Shirt = 2,
-        Shoes = 3,
-    }
-    enum Size
-    {
-        Xs = 1,
-        S = 2,
-        M = 3,
-    }
-    enum Color
-    {
-        Blue = 1,
-        Green = 2,
-        Yellow = 3,
-    }
-    enum Price
-    {
-        OneHundred = 100,
-        TwoHundred = 200,
-        ThreeHundred = 300,
+            for (int i = 0; i < length; i++)
+            {
+                int a = randomID.Next(26);
+                random = random + b.ElementAt(a);
+            }
+            return random;
+            //Console.WriteLine($"L-{random}");
+        }
     }
 }
